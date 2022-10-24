62f72a01ac2c01ff4aa98f1285c416570dee028e
Tonelive <bruno.scastro2012@hotmail.com>
2022-10-24 15:42:58 -0300

    chore: add tailwind css

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-between items-center h-12 bg-fuchsia-300">
+      <div className="flex">
+
+      <p>Icone</p>
+      <div>
+        <button>Botão 1</button>
+        <button>Botão 2</button>
+      </div>
+      </div>
+
+      <p className="text-white text-lg font-bold">Taura Bots</p>
+
+      <div>
+        Login
+      </div>
+    </div>
+  )
+}
